f25b78d17d6d5e30ab771ab37b8591942ab103a9
Lennaben <BENALOUILENNA@GMAIL.COM>
2023-08-10 10:55:12 +0200

    formulaire ok

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,28 +1,32 @@
-import { useCallback, useState } from "react";
-import PropTypes from "prop-types";
-import Field, { FIELD_TYPES } from "../../components/Field";
-import Select from "../../components/Select";
-import Button, { BUTTON_TYPES } from "../../components/Button";
+import { useCallback, useState } from "react"
+import PropTypes from "prop-types"
+import Field, { FIELD_TYPES } from "../../components/Field"
+import Select from "../../components/Select"
+import Button, { BUTTON_TYPES } from "../../components/Button"
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000)
+  })
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState(false);
+  const [sending, setSending] = useState(false)
   const sendContact = useCallback(
     async (evt) => {
-      evt.preventDefault();
-      setSending(true);
+      evt.preventDefault()
+      setSending(true)
       // We try to call mockContactApi
       try {
-        await mockContactApi();
-        setSending(false);
+        await mockContactApi()
+        onSuccess()
+        setSending(false)
       } catch (err) {
-        setSending(false);
-        onError(err);
+        setSending(false)
+        onError(err)
       }
     },
     [onSuccess, onError]
-  );
+  )
   return (
     <form onSubmit={sendContact}>
       <div className="row">
@@ -50,8 +54,8 @@ const Form = ({ onSuccess, onError }) => {
         </div>
       </div>
     </form>
-  );
-};
+  )
+}
 
 Form.propTypes = {
   onError: PropTypes.func,
@@ -63,4 +67,4 @@ Form.defaultProps = {
   onSuccess: () => null,
 }
 
-export default Form;
+export default Form
